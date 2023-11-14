968503f69d723c2555380df8244f53ace524dbca
Bhandavi Chukka <141643111+Chbhandavi@users.noreply.github.com>
2023-11-14 17:07:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+const api = {
+  key: "b15ded75f2264ce7d3e9d3a43b5df1cd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keydown", setQuery);
+
+function setQuery(evt) {
+  if (evt.key === "Enter") {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults)
+    .catch((error) => {
+      console.error('Error fetching weather data:', error);
+      alert('Error fetching weather data. Please try again.');
+    });
+}
+
+function displayResults(weather) {
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+  let weather_el = document.querySelector(".current .weather");
+  weather_el.innerText = weather.weather[0].main;
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
